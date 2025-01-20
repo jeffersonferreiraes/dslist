527b94682ec0fd16060719a3c79d6ab7d0e388a4
Jefferson Alves <jealves359@gmail.com>
2025-01-20 12:32:38 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -21,23 +21,27 @@ public class Game {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String platfroms;
+	private String platforms;
 	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
 		
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String platfroms, Double score, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platfroms = platfroms;
+		this.platforms = platforms;
 		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
@@ -76,12 +80,12 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public String getPlatfroms() {
-		return platfroms;
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlatfroms(String platfroms) {
-		this.platfroms = platfroms;
+	public void setPlatfroms(String platforms) {
+		this.platforms = platforms;
 	}
 
 	public Double getScore() {
